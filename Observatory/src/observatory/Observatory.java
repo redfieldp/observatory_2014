d7169e7c5b29df4450445d5331fe02242619da1b
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-02-28 19:43:10 -0800

    Key pressed listeners and method stubs.

--- Observatory/src/observatory/Observatory.java
+++ Observatory/src/observatory/Observatory.java
@@ -7,9 +7,101 @@ public class Observatory extends PApplet {
     String dataUrl = "";
     DataFeed currentDataFeed = new DataFeed(dataUrl);
     
+    boolean performancePaused = false;
+    
 	public void setup() {
 	}
 
 	public void draw() {
+	    if (!performancePaused) {
+	        
+	    }
 	}
+	
+	public void keyPressed() {
+	    if (key == '+') {
+	        increaseLargeThreshold();
+	    }
+	    else if (key == '-'){
+	        decreaseLargeThreshold();
+	    }
+        else if (key == '9'){
+            decreaseMediumThreshold();
+        }
+        else if (key == '0'){
+            increaseMediumThreshold();
+        }
+        else if (key == 'T'){
+            switchToNextTemplate();
+        }
+        else if (key == ' '){
+            togglePause();
+        }
+        else if (key == 'P'){
+            savePDF();
+        }
+        else if (key == 'L'){
+            toggleLiveData();
+        }
+        else if (key == 'F'){
+            toggleFullScreen();
+        }
+        else if (key == 'Q'){
+            exit();
+        }
+	}
+
+    private void savePDF()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void toggleFullScreen()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void toggleLiveData()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void togglePause()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void increaseMediumThreshold()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void switchToNextTemplate()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void decreaseMediumThreshold()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void decreaseLargeThreshold()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void increaseLargeThreshold()
+    {
+        // TODO Auto-generated method stub
+        
+    }
 }
