22b883c54fe47ae4f98d179467ee50f8a8a1f8bf
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-02-28 16:30:37 -0800

    Data point and envelope objects.

---
+++ Observatory/src/observatory/DataEnvelope.java
@@ -0,0 +1,11 @@
+package observatory;
+
+public class DataEnvelope
+{
+    int duration, deltaMagnitude;
+    float angle;
+    DataEnvelope (DataPoint newPoint, DataPoint oldPoint) {
+        // Calculate relative values and populate variables
+        
+    }
+}
---
+++ Observatory/src/observatory/DataPoint.java
@@ -0,0 +1,32 @@
+package observatory;
+
+import java.util.Date;
+
+public class DataPoint
+{
+    Date time = new Date();
+
+    double originalMagnitude = 0.0;
+
+    int magnitude;
+    
+    DataPoint lastBigPoint;
+
+    DataPoint lastMediumPoint;
+
+    DataEnvelope peakEnvelope;
+
+    DataEnvelope smoothedEnvelope;
+    
+    public DataPoint(String dataPoint, DataPoint lastBig, DataPoint lastMedium) {
+        // Parse Data point
+        
+        // Grab other data point arguments
+        lastBigPoint = lastBig;
+        lastMediumPoint = lastMedium;
+        
+        // Create envelopes using points
+        peakEnvelope = new DataEnvelope(this, lastBigPoint);
+        smoothedEnvelope = new DataEnvelope(this, lastMediumPoint);
+    }
+}
