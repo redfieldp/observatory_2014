cf61279dbe753044552de080a3fb86e6591587a4
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-03-02 10:16:31 -0800

    Timers for grabbing data and rotating templates

--- Observatory/src/observatory/Observatory.java
+++ Observatory/src/observatory/Observatory.java
@@ -1,6 +1,8 @@
 package observatory;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import processing.core.PApplet;
 
@@ -13,20 +15,30 @@ public class Observatory extends PApplet {
     ArrayList<DataPoint> storedDataPoints = new ArrayList<DataPoint>();
     ArrayList<ObservatoryLine> lines = new ArrayList<ObservatoryLine>();
     Template[] templates = {new RainTemplate(), new ToothpicksTemplate(), new ClusteredRightTemplate(), new ClusteredLeftTemplate()};
+    Timer dataGrabber;
+    Timer templateSwitcher;
 
     boolean performancePaused = false;
     boolean useStoredData = false;
     boolean fullScreenMode = false;
     boolean saveDataToFile = false;
+    
     int maxNumberOfLines = 100;
     int rotateTemplateDuration = 20;
     int thresholdIncrement = 10;
     int canvasHeight = 480;
     int canvasWidth = 640;
-    float thicknessUnit = 0.0001f;
+    int dataUpdateFrequency = 10;
+    int templateRotationCount = 0;
     
+    float thicknessUnit = 0.0001f;
+
     public void setup() {
         size(canvasWidth, canvasHeight);
+
+        // Schedule the timers
+        dataTimerSetup();
+        templateTimerSetup();
     }
 
     public void draw() {
@@ -81,8 +93,7 @@ public class Observatory extends PApplet {
 
     private void toggleFullScreen()
     {
-        // TODO Auto-generated method stub
-
+        fullScreenMode = !fullScreenMode;
     }
 
     private void toggleLiveData()
@@ -125,5 +136,34 @@ public class Observatory extends PApplet {
     {
         // TODO Auto-generated method stub
 
+    }
+
+    public void dataTimerSetup() {
+        dataGrabber = new Timer();
+        dataGrabber.schedule(new GrabDataTask(), 0, dataUpdateFrequency * 1000);
+    }
+    
+    class GrabDataTask extends TimerTask {
+        public void run() {
+            if (!useStoredData) {
+                
+            }
+        }
+    }
+    
+    public void templateTimerSetup() {
+        templateSwitcher = new Timer();
+        templateSwitcher.schedule(new TemplateRotationTask(), 0, rotateTemplateDuration * 1000);
+    }
+
+    class TemplateRotationTask extends TimerTask {
+        public void run() {
+            templateRotationCount = (templateRotationCount + 1) % templates.length;
+            currentTemplate = templates[templateRotationCount];
+        }
+    }
+    
+    public boolean sketchFullScreen() {
+        return fullScreenMode;
     }
 }
