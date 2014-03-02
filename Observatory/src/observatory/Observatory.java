60386c981c9a23071f562d8d2468098b6ca1245e
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-03-01 21:06:53 -0800

    Main class variables and objects.

--- Observatory/src/observatory/Observatory.java
+++ Observatory/src/observatory/Observatory.java
@@ -1,30 +1,52 @@
 package observatory;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 
 public class Observatory extends PApplet {
     String dataUrl = "";
     DataFeed currentDataFeed = new DataFeed(dataUrl);
-    
-    boolean performancePaused = false;
-    
-	public void setup() {
-	}
+    Template currentTemplate;
+    ArrayList<DataPoint> incomingData = new ArrayList<DataPoint>();
+    ArrayList<DataPoint> storedDataPoints = new ArrayList<DataPoint>();
+    ArrayList<ObservatoryLine> lines = new ArrayList<ObservatoryLine>();
+    Template[] templates = {new RainTemplate(), new ToothpicksTemplate(), new ClusteredRightTemplate(), new ClusteredLeftTemplate()};
 
-	public void draw() {
-	    if (!performancePaused) {
-	        
-	    }
-	}
-	
-	public void keyPressed() {
-	    if (key == '+') {
-	        increaseLargeThreshold();
-	    }
-	    else if (key == '-'){
-	        decreaseLargeThreshold();
-	    }
+    boolean performancePaused = false;
+    boolean useStoredData = false;
+    boolean fullScreenMode = false;
+    boolean saveDataToFile = false;
+    int maxNumberOfLines = 100;
+    int rotateTemplateDuration = 20;
+    int thresholdIncrement = 10;
+    int canvasHeight = 480;
+    int canvasWidth = 640;
+    float thicknessUnit = 0.0001f;
+    
+    public void setup() {
+        size(canvasWidth, canvasHeight);
+    }
+
+    public void draw() {
+        if (!performancePaused) {
+            if (useStoredData) {
+
+            }
+            else {
+
+            }
+        }
+    }
+
+    public void keyPressed() {
+        if (key == '+') {
+            increaseLargeThreshold();
+        }
+        else if (key == '-'){
+            decreaseLargeThreshold();
+        }
         else if (key == '9'){
             decreaseMediumThreshold();
         }
@@ -49,59 +71,59 @@ public class Observatory extends PApplet {
         else if (key == 'Q'){
             exit();
         }
-	}
+    }
 
     private void savePDF()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void toggleFullScreen()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void toggleLiveData()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void togglePause()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void increaseMediumThreshold()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void switchToNextTemplate()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void decreaseMediumThreshold()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void decreaseLargeThreshold()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
     private void increaseLargeThreshold()
     {
         // TODO Auto-generated method stub
-        
+
     }
 }
