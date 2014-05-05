cbc8de779f965192be921f344f36abdf03ad1997
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-05-05 16:46:04 -0400

    Added rolling threshold calc to RecentData.

--- Observatory/src/observatory/RecentData.java
+++ Observatory/src/observatory/RecentData.java
@@ -8,99 +8,144 @@ import observatory.Observatory.GrabDataTask;
 
 public class RecentData
 {
-	int recentDataDuration = 100;
+    int recentDataDuration = 100;
 
-	ArrayList<DataPoint> listOfDataPoints = new ArrayList<DataPoint>();
+    ArrayList<DataPoint> listOfDataPoints = new ArrayList<DataPoint>();
 
-	int maximumDataPoints = 6000;
+    int maximumDataPoints = 6000;
 
-	private int recentAverageOfBigShakes = 0;
+    private int recentAverageOfBigShakes = 0;
 
-	private int recentAverageOfMediumShakes = 0;
+    private int recentAverageOfMediumShakes = 0;
 
-	private int recentAverageOfSmallShakes = 0;
+    private int recentAverageOfSmallShakes = 0;
 
-	private int recentAverageOfAllShakes  = 0;
+    private int recentAverageOfAllShakes  = 0;
 
-	int recentPeak = 0;
+    int recentPeak = 0;
 
-	Timer calculationTimer;
+    Timer calculationTimer, thresholdTimer;
 
-	int thresholdLarge = 25, thresholdMedium = 5;
+    int thresholdRecalcInterval = 60000, thresholdCalcScope = 600000;
+    int drawnLinesPerSecond = 5; 
+    int thresholdLineLimit = (thresholdCalcScope/1000) * drawnLinesPerSecond;
 
-	public RecentData() {
-		calculationTimer = new Timer();
-		calculationTimer.schedule(new RecentDataCalculation(), 0, 5000);
-	}
+    int thresholdLarge = 25, thresholdMedium = 5;
 
-	public int getBigShakesAverage() {
-		return recentAverageOfBigShakes;
-	}
+    public RecentData() {
+        calculationTimer = new Timer();
+        calculationTimer.schedule(new RecentDataCalculation(), 0, 5000);
+        thresholdTimer = new Timer();
+        thresholdTimer.schedule(new ThresholdCalculation(), 0, thresholdRecalcInterval);
+    }
 
-	public int getMediumShakesAverage() {
-		return recentAverageOfMediumShakes;
-	}
+    public int getBigShakesAverage() {
+        return recentAverageOfBigShakes;
+    }
 
-	public int getSmallShakesAverage() {
-		return recentAverageOfSmallShakes;
-	}
+    public int getMediumShakesAverage() {
+        return recentAverageOfMediumShakes;
+    }
 
-	public int getAllShakesAverage() {
-		return recentAverageOfAllShakes;
-	}
+    public int getSmallShakesAverage() {
+        return recentAverageOfSmallShakes;
+    }
 
-	public void addDataPoint(DataPoint p) {
-		synchronized(listOfDataPoints) {
-			if (listOfDataPoints.size() ==  maximumDataPoints) {
-				// Remove the oldest point if we have too many
-				listOfDataPoints.remove(0);
-			}
-			listOfDataPoints.add(p);
-		}
-	}
+    public int getAllShakesAverage() {
+        return recentAverageOfAllShakes;
+    }
 
-	public void saveData(){
+    public void addDataPoint(DataPoint p) {
+        synchronized(listOfDataPoints) {
+            if (listOfDataPoints.size() ==  maximumDataPoints) {
+                // Remove the oldest point if we have too many
+                listOfDataPoints.remove(0);
+            }
+            listOfDataPoints.add(p);
+        }
+    }
 
-	}
-	
-	public void setLargeThreshold(int lt) {
-		thresholdLarge = lt;
-	}
-	
-	public void setMediumThreshold(int mt) {
-		thresholdMedium = mt;
-	}
+    public void saveData(){
 
-	class RecentDataCalculation extends TimerTask {
-		public void run() {
-			int counter = 0;
-			if (listOfDataPoints.size() > 0) {
-				synchronized(listOfDataPoints) {
-					recentAverageOfBigShakes = 0;
-					recentAverageOfMediumShakes = 0;
-					recentAverageOfSmallShakes = 0;
-					recentAverageOfAllShakes = 0;
-					for (DataPoint d : listOfDataPoints) {
-						recentAverageOfAllShakes += d.magnitude;
-						counter++;
+    }
 
-						if (d.magnitude > thresholdLarge) {
-							recentAverageOfBigShakes += d.magnitude;
-						}
-						else if (d.magnitude > thresholdMedium) {
-							recentAverageOfMediumShakes += d.magnitude;
-						}
-						else{
-							recentAverageOfSmallShakes += d.magnitude;
-						}
-					}
+    public void setLargeThreshold(int lt) {
+        thresholdLarge = lt;
+    }
 
-					recentAverageOfBigShakes = recentAverageOfBigShakes/counter;
-					recentAverageOfMediumShakes = recentAverageOfMediumShakes/counter;
-					recentAverageOfSmallShakes = recentAverageOfSmallShakes/counter;
-					recentAverageOfAllShakes = recentAverageOfAllShakes/counter;
-				}
-			}
-		}
-	}
+    public void setMediumThreshold(int mt) {
+        thresholdMedium = mt;
+    }
+
+    class RecentDataCalculation extends TimerTask {
+        public void run() {
+            int counter = 0;
+            if (listOfDataPoints.size() > 0) {
+                synchronized(listOfDataPoints) {
+                    recentAverageOfBigShakes = 0;
+                    recentAverageOfMediumShakes = 0;
+                    recentAverageOfSmallShakes = 0;
+                    recentAverageOfAllShakes = 0;
+                    for (DataPoint d : listOfDataPoints) {
+                        recentAverageOfAllShakes += d.magnitude;
+                        counter++;
+
+                        if (d.magnitude > thresholdLarge) {
+                            recentAverageOfBigShakes += d.magnitude;
+                        }
+                        else if (d.magnitude > thresholdMedium) {
+                            recentAverageOfMediumShakes += d.magnitude;
+                        }
+                        else{
+                            recentAverageOfSmallShakes += d.magnitude;
+                        }
+                    }
+
+                    recentAverageOfBigShakes = recentAverageOfBigShakes/counter;
+                    recentAverageOfMediumShakes = recentAverageOfMediumShakes/counter;
+                    recentAverageOfSmallShakes = recentAverageOfSmallShakes/counter;
+                    recentAverageOfAllShakes = recentAverageOfAllShakes/counter;
+                }
+            }
+        }
+    }
+
+    class ThresholdCalculation extends TimerTask {
+        public void run() {
+            ArrayList<DataPoint> thresholdPoints = new ArrayList<DataPoint>();
+            if (listOfDataPoints.size() > 0) {
+                synchronized(listOfDataPoints) {
+                    for (DataPoint d : listOfDataPoints) {
+                        if (System.currentTimeMillis()  - d.time < thresholdCalcScope) {
+                            if (thresholdPoints.size() < thresholdLineLimit) {
+                                thresholdPoints.add(d);
+                            }
+                            else {
+                                DataPoint toRemove = null;
+                                for (DataPoint d2 : thresholdPoints) {
+                                    if (d.magnitude > d2.magnitude) {
+                                        toRemove = d2;
+                                        break;
+                                    }
+                                }
+                                if (toRemove != null) {
+                                    thresholdPoints.remove(toRemove);
+                                    thresholdPoints.add(d);
+                                }
+                            }
+                        }
+                    }
+
+                    int newThreshold = 1000000;
+                    for (DataPoint d : thresholdPoints) {
+                        if (d.magnitude < newThreshold) {
+                            newThreshold = (int)d.magnitude;
+                        }
+                    }
+
+                    thresholdLarge = newThreshold;
+                }
+            }
+        }
+    }
 }
