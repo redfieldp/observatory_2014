77ee04a669c439e71daa013b9e794b9546e5d67a
EricTimLaptop <eric@ericlopresti.com>
2015-05-16 19:17:46 -0400

    S from Tim

--- Observatory/src/observatory/Observatory.java
+++ Observatory/src/observatory/Observatory.java
@@ -19,7 +19,7 @@ public class Observatory extends PApplet {
 	
     //PERFORMANCE VARS
     //Most are adjustable w keyboard
-//EFGHIJKIJKLMNOPQR
+//EFGHIJKIJKLMNOPQRS
 
     boolean performancePaused = false;
     boolean useStoredData = false; // If true, we run in 'prerecorded mode,' using data from 
