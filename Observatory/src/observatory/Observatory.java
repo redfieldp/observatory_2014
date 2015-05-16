e5c9c071566c831c5ace985c7de562b48bbb7fe7
ericlopresti <eric@ericlopresti.com>
2015-05-16 15:48:48 -0400

    A!

--- Observatory/src/observatory/Observatory.java
+++ Observatory/src/observatory/Observatory.java
@@ -19,7 +19,7 @@ public class Observatory extends PApplet {
 	
     //PERFORMANCE VARS
     //Most are adjustable w keyboard
-    //Another Test from Tsuki
+    //Another Test from Tsuki A!
 	//Test from PC
 
     boolean performancePaused = false;
