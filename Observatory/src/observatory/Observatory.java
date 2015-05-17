916ba0d407b5d7a257b7bc0f7abcb438a33b708d
EricTimLaptop <eric@ericlopresti.com>
2015-05-17 15:55:42 -0400

    V from Tim, by REMOTE!

--- Observatory/src/observatory/Observatory.java
+++ Observatory/src/observatory/Observatory.java
@@ -17,7 +17,7 @@ import java.awt.datatransfer.*;
 
 public class Observatory extends PApplet {
 	
-    // Test Update #TU
+    // Test Update #TUV
 	
 	//PERFORMANCE VARS
     //Most are adjustable w keyboard
